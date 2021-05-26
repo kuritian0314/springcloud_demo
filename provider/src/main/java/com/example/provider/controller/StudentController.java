d208bc58981e0a6b45e41ebaa8ebc2f3090f3599
weihua <1318022435@qq.com>
2021-05-26 14:28:04 +0800

    "版本1：查看代码"

---
+++ provider/src/main/java/com/example/provider/controller/StudentController.java
@@ -0,0 +1,43 @@
+package com.example.provider.controller;
+
+import com.example.provider.entity.Student;
+import com.example.provider.repository.StudentRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Collection;
+
+@RestController
+@RequestMapping("/stu")
+public class StudentController {
+
+    @Autowired
+    private StudentRepo studentRepo;
+
+    @GetMapping("/findAll")
+    public Collection<Student> findAll(){
+        return studentRepo.findAll();
+    }
+
+    @GetMapping("/findById/{id}")
+    public Student findById(@PathVariable("id") Integer id){
+        return studentRepo.findById(id);
+    }
+
+    @PostMapping("/save")
+    public void save(@RequestBody Student student){
+        studentRepo.update(student);
+    }
+
+    @PutMapping("/update")
+    public void update(@RequestBody Student student){
+        studentRepo.update(student);
+    }
+
+    @DeleteMapping("/deleteById/{id}")
+    public void deleteById(@PathVariable("id") Integer id){
+        studentRepo.deleteById(id);
+    }
+
+}
+
